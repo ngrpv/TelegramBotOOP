832c98b6a5e48cc8d1b5f615517ad0b7d38e0ffb
ngrpv <nazar.garipov475@gmail.com>
2021-09-22 19:56:37 +0500

    Improved messages to user

--- src/main/java/HangmanGame.java
+++ src/main/java/HangmanGame.java
@@ -1,15 +1,14 @@
-import org.javatuples.Pair;
-
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Locale;
 
 public class HangmanGame implements IGame {
     private String word;
     private HashSet<Character> wordHashSet;
+    private HashSet<Character> guessedLetters = new HashSet<>();
     private HashSet<Character> usedLetters = new HashSet<>();
     private String WIN_TEXT = "Ты выиграл!";
-    private int attemptsCount = 0;
+    private String ALREADY_USED_LETTER = "Буква была введена ранее, введите другую!";
+    private int unsuccessfulAttemptsCount = 0;
 
     public HangmanGame(String word) {
         this.word = word;
@@ -20,21 +19,34 @@ public class HangmanGame implements IGame {
     public String checkAnswer(String answer) {
         if (answer.length() != 1) return "Ты должен написать только одну букву!";
         var userChar = answer.toLowerCase(Locale.ROOT).charAt(0);
+        if (usedLetters.contains(userChar)) {
+            return ALREADY_USED_LETTER;
+        }
+        usedLetters.add(userChar);
         if (wordHashSet.contains(userChar))
-            usedLetters.add(userChar);
-        if (wordHashSet.size() == usedLetters.size()) {
+            guessedLetters.add(userChar);
+        else unsuccessfulAttemptsCount++;
+        if (wordHashSet.size() == guessedLetters.size()) {
             return getWordWithGuessedLetters() + "\n" + WIN_TEXT;
         }
-        if (++attemptsCount > 5) {
+        if (unsuccessfulAttemptsCount > 5) {
             return "Ты проиграл!";
         }
-        return getWordWithGuessedLetters() + "\n" + "Отсалось попыток: " + (6 - attemptsCount);
+        return getWordWithGuessedLetters() + "\n" + "Отсалось жизней: " + (6 - unsuccessfulAttemptsCount);
+    }
+
+    public Boolean isWin() {
+        return wordHashSet.size() == guessedLetters.size();
+    }
+
+    public String getHiddenWord() {
+        return getWordWithGuessedLetters();
     }
 
     private String getWordWithGuessedLetters() {
         var resultStr = new StringBuilder();
         for (Character ch : word.toCharArray()) {
-            if (usedLetters.contains(ch)) {
+            if (guessedLetters.contains(ch)) {
                 resultStr.append(ch).append(" ");
             } else {
                 resultStr.append("* ");
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String[] args){
         var game = new HangmanGame("барабан");
         var scanner = new Scanner(System.in);
-        while(true){
+        System.out.println(game.getHiddenWord());
+        while(scanner.hasNext()){
             var userMessage = scanner.nextLine();
+            if(userMessage.length() == 0) continue;
             if(userMessage.equals("exit")) break;
             System.out.println(game.checkAnswer(userMessage));
+            if(game.isWin()){
+                game = new HangmanGame("sadsadas");
+            }
         }
     }
 }
