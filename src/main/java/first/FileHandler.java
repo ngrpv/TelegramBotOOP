530be6c2947f19e7b36456fd280d33429a7b57cb
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2021-10-10 19:03:01 +0500

    add new words to file

--- src/main/java/first/FileHandler.java
+++ src/main/java/first/FileHandler.java
@@ -9,7 +9,7 @@ public class FileHandler implements IWordParser{
     Scanner fileScanner;
 
     public  FileHandler() throws FileNotFoundException {
-        fileScanner = new Scanner(new BufferedReader(new FileReader("newWords.txt")));
+        fileScanner = new Scanner(new BufferedReader(new FileReader("hangmanWords.txt")));
     }
     
     public String getNextWord(){
