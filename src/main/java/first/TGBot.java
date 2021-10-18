90ee1633cec15e732ef392e268b871690826ba49
ngrpv <nazar.garipov475@gmail.com>
2021-10-18 18:08:58 +0500

    Refactor TGBot

--- src/main/java/first/TGBot.java
+++ src/main/java/first/TGBot.java
@@ -4,13 +4,15 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 import java.util.HashMap;
+import java.util.List;
 
 public class TGBot extends TelegramLongPollingBot {
-    private String userName;
-    private HashMap<Long, User> userStates = new HashMap<>();
+    private final String userName;
+    private final HashMap<Long, User> userStates = new HashMap<>();
 
     public TGBot(String userName) {
         this.userName = userName;
@@ -32,24 +34,16 @@ public class TGBot extends TelegramLongPollingBot {
         var userState = getUserState(chatId);
         var messageText = update.getMessage().getText();
         var sendMessage = new SendMessage();
+
         sendMessage.setChatId(update.getMessage().getChatId().toString());
         sendMessage.setText(BotLogic.handleMessage(messageText, userState));
-        if(userState.stateIsChanged) {
-            var replyMarkup = new ReplyKeyboardMarkup();
-            new TGBotButtons();
-            replyMarkup.setKeyboard(TGBotButtons.getButtons(userState.state));
-            replyMarkup.setResizeKeyboard(true);
-            replyMarkup.setOneTimeKeyboard(true);
-            sendMessage.setReplyMarkup(replyMarkup);
+
+        if (userState.stateIsChanged) {
+            setKeyboard(sendMessage, TGBotButtons.getButtons(userState.state));
             userState.stateIsChanged = false;
         }
 
-
-        try {
-            execute(sendMessage);
-        } catch (TelegramApiException e) {
-            e.printStackTrace();
-        }
+        trySend(sendMessage);
     }
 
     public User getUserState(Long chatId) {
@@ -63,5 +57,20 @@ public class TGBot extends TelegramLongPollingBot {
         return user;
     }
 
+    private void trySend(SendMessage message) {
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
 
+    private void setKeyboard(SendMessage sendMessage, List<KeyboardRow> keyboardRows) {
+        var replyMarkup = new ReplyKeyboardMarkup();
+        new TGBotButtons();
+        replyMarkup.setKeyboard(keyboardRows);
+        replyMarkup.setResizeKeyboard(true);
+        replyMarkup.setOneTimeKeyboard(true);
+        sendMessage.setReplyMarkup(replyMarkup);
+    }
 }
