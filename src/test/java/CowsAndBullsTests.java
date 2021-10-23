5435fdc1db27391fa2c488d07462cc5b82d564fe
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2021-10-23 14:20:28 +0500

    Refactoring

--- src/test/java/CowsAndBullsTests.java
+++ src/test/java/CowsAndBullsTests.java
@@ -1,5 +1,6 @@
 import first.cowsAndBulls.CowsAndBullsState;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class CowsAndBullsTests {
@@ -13,16 +14,6 @@ public class CowsAndBullsTests {
         Assertions.assertSame(message, WORD_EQUAL_LENGTH);
     }
 
-    @Test
-    public void check_correct_count_cows() {
-        var game = new CowsAndBullsState();
-        game.setWord("жигало");
-        var bulls = game.checkAnswer("гигант").toCharArray();
-        var cows = game.checkAnswer("гигант").toCharArray();
-        Assertions.assertEquals(bulls[5], '3');
-        Assertions.assertEquals(cows[14], '0');
-    }
-
     public int[] getCowsAndBulls(String userWord, String word) {
         var game = new CowsAndBullsState();
         game.setWord(word);
@@ -33,101 +24,70 @@ public class CowsAndBullsTests {
 
     @Test
     public void check_correct_count_cowsAndBulls() {
-        var bulls = getCowsAndBulls("гигант", "жигало")[0];
-        var cows = getCowsAndBulls("гигант", "жигало")[1];
-        Assertions.assertEquals(bulls, 3);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(3,getCowsAndBulls("гигант","жигало")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("гигант","жигало")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls2() {
-        var bulls = getCowsAndBulls("гигант", "город")[0];
-        var cows = getCowsAndBulls("гигант", "город")[1];
-        Assertions.assertEquals(bulls, 1);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(1,getCowsAndBulls("гигант","город")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("гигант","город")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls3() {
-        var bulls = getCowsAndBulls("катп", "анас")[0];
-        var cows = getCowsAndBulls("катп", "анас")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 1);
+        Assertions.assertEquals(0,getCowsAndBulls("катп","анас")[0]);
+        Assertions.assertEquals(1,getCowsAndBulls("катп","анас")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls4() {
-        var bulls = getCowsAndBulls("палка", "лейка")[0];
-        var cows = getCowsAndBulls("палка", "лейка")[1];
-        Assertions.assertEquals(bulls, 2);
-        Assertions.assertEquals(cows, 1);
+        Assertions.assertEquals(2,getCowsAndBulls("палка","лейка")[0]);
+        Assertions.assertEquals(1,getCowsAndBulls("палка","лейка")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls5() {
-        var bulls = getCowsAndBulls("окурок", "крутой")[0];
-        var cows = getCowsAndBulls("окурок", "крутой")[1];
-        Assertions.assertEquals(bulls, 2);
-        Assertions.assertEquals(cows, 2);
+        Assertions.assertEquals(2,getCowsAndBulls("окурок","крутой")[0]);
+        Assertions.assertEquals(2,getCowsAndBulls("окурок","крутой")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls6() {
-        var bulls = getCowsAndBulls("лепс", "спел")[0];
-        var cows = getCowsAndBulls("лепс", "спел")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 4);
+        Assertions.assertEquals(0,getCowsAndBulls("лепс","спел")[0]);
+        Assertions.assertEquals(4,getCowsAndBulls("лепс","спел")[1]);
     }
 
     @Test
     public void check_correct_string_of_empty_char() {
-        var bulls = getCowsAndBulls("    ", "спел")[0];
-        var cows = getCowsAndBulls("    ", "спел")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(0,getCowsAndBulls("    ","спел")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("    ","спел")[1]);
     }
 
     @Test
     public void check_correct_count_cowsAndBulls7() {
-        var bulls = getCowsAndBulls("5522", "2255")[0];
-        var cows = getCowsAndBulls("5522", "2255")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 4);
+        Assertions.assertEquals(0,getCowsAndBulls("5522","2255")[0]);
+        Assertions.assertEquals(4,getCowsAndBulls("5522","2255")[1]);
     }
     @Test
     public void check_correct_count_cowsAndBulls8() {
-        var bulls = getCowsAndBulls("2222", "2255")[0];
-        var cows = getCowsAndBulls("2222", "2255")[1];
-        Assertions.assertEquals(bulls, 2);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(2,getCowsAndBulls("2222","2255")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("2222","2255")[1]);
     }
     @Test
     public void check_correct_count_cowsAndBulls9() {
-        var bulls = getCowsAndBulls("0003", "1323")[0];
-        var cows = getCowsAndBulls("0003", "1323")[1];
-        Assertions.assertEquals(bulls, 1);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(1,getCowsAndBulls("0003","1323")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("0003","1323")[1]);
     }
     @Test
     public void check_correct_count_cowsAndBulls10() {
-        var bulls = getCowsAndBulls("00002", "1112")[0];
-        var cows = getCowsAndBulls("00002", "1112")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 1);
+        Assertions.assertEquals(0,getCowsAndBulls("00002","1112")[0]);
+        Assertions.assertEquals(1,getCowsAndBulls("00002","1112")[1]);
     }
     @Test
     public void check_correct_count_cowsAndBulls11() {
-        var bulls = getCowsAndBulls("00002", "1112")[0];
-        var cows = getCowsAndBulls("00002", "1112")[1];
-        Assertions.assertEquals(bulls, 0);
-        Assertions.assertEquals(cows, 1);
-    }
-    @Test
-    public void check_correct_count_cowsAndBulls12() {
-        var bulls = getCowsAndBulls("0209", "0009")[0];
-        var cows = getCowsAndBulls("0209", "0009")[1];
-        Assertions.assertEquals(bulls, 3);
-        Assertions.assertEquals(cows, 0);
+        Assertions.assertEquals(3,getCowsAndBulls("0209","0009")[0]);
+        Assertions.assertEquals(0,getCowsAndBulls("0209","0009")[1]);
     }
 
 
