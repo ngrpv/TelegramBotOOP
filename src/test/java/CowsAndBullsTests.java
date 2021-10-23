89149b145f2912e9553358b7476488399cdb7585
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2021-10-23 15:42:15 +0500

    Refactoring

--- src/test/java/CowsAndBullsTests.java
+++ src/test/java/CowsAndBullsTests.java
@@ -1,6 +1,5 @@
 import first.cowsAndBulls.CowsAndBullsState;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class CowsAndBullsTests {
@@ -24,71 +23,31 @@ public class CowsAndBullsTests {
 
     @Test
     public void check_correct_count_cowsAndBulls() {
-        Assertions.assertEquals(3,getCowsAndBulls("гигант","жигало")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("гигант","жигало")[1]);
+        Assertions.assertAll(
+                () -> Assertions.assertEquals(3, getCowsAndBulls("гигант", "жигало")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("гигант", "жигало")[1]),
+                () -> Assertions.assertEquals(1, getCowsAndBulls("гигант", "город")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("гигант", "город")[1]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("катп", "анас")[0]),
+                () -> Assertions.assertEquals(1, getCowsAndBulls("катп", "анас")[1]),
+                () -> Assertions.assertEquals(2, getCowsAndBulls("палка", "лейка")[0]),
+                () -> Assertions.assertEquals(1, getCowsAndBulls("палка", "лейка")[1]),
+                () -> Assertions.assertEquals(2, getCowsAndBulls("окурок", "крутой")[0]),
+                () -> Assertions.assertEquals(2, getCowsAndBulls("окурок", "крутой")[1]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("лепс", "спел")[0]),
+                () -> Assertions.assertEquals(4, getCowsAndBulls("лепс", "спел")[1]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("    ", "спел")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("    ", "спел")[1]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("5522", "2255")[0]),
+                () -> Assertions.assertEquals(4, getCowsAndBulls("5522", "2255")[1]),
+                () -> Assertions.assertEquals(2, getCowsAndBulls("2222", "2255")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("2222", "2255")[1]),
+                () -> Assertions.assertEquals(1, getCowsAndBulls("0003", "1323")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("0003", "1323")[1]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("00002", "1112")[0]),
+                () -> Assertions.assertEquals(1, getCowsAndBulls("00002", "1112")[1]),
+                () -> Assertions.assertEquals(3, getCowsAndBulls("0209", "0009")[0]),
+                () -> Assertions.assertEquals(0, getCowsAndBulls("0209", "0009")[1]));
     }
-
-    @Test
-    public void check_correct_count_cowsAndBulls2() {
-        Assertions.assertEquals(1,getCowsAndBulls("гигант","город")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("гигант","город")[1]);
-    }
-
-    @Test
-    public void check_correct_count_cowsAndBulls3() {
-        Assertions.assertEquals(0,getCowsAndBulls("катп","анас")[0]);
-        Assertions.assertEquals(1,getCowsAndBulls("катп","анас")[1]);
-    }
-
-    @Test
-    public void check_correct_count_cowsAndBulls4() {
-        Assertions.assertEquals(2,getCowsAndBulls("палка","лейка")[0]);
-        Assertions.assertEquals(1,getCowsAndBulls("палка","лейка")[1]);
-    }
-
-    @Test
-    public void check_correct_count_cowsAndBulls5() {
-        Assertions.assertEquals(2,getCowsAndBulls("окурок","крутой")[0]);
-        Assertions.assertEquals(2,getCowsAndBulls("окурок","крутой")[1]);
-    }
-
-    @Test
-    public void check_correct_count_cowsAndBulls6() {
-        Assertions.assertEquals(0,getCowsAndBulls("лепс","спел")[0]);
-        Assertions.assertEquals(4,getCowsAndBulls("лепс","спел")[1]);
-    }
-
-    @Test
-    public void check_correct_string_of_empty_char() {
-        Assertions.assertEquals(0,getCowsAndBulls("    ","спел")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("    ","спел")[1]);
-    }
-
-    @Test
-    public void check_correct_count_cowsAndBulls7() {
-        Assertions.assertEquals(0,getCowsAndBulls("5522","2255")[0]);
-        Assertions.assertEquals(4,getCowsAndBulls("5522","2255")[1]);
-    }
-    @Test
-    public void check_correct_count_cowsAndBulls8() {
-        Assertions.assertEquals(2,getCowsAndBulls("2222","2255")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("2222","2255")[1]);
-    }
-    @Test
-    public void check_correct_count_cowsAndBulls9() {
-        Assertions.assertEquals(1,getCowsAndBulls("0003","1323")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("0003","1323")[1]);
-    }
-    @Test
-    public void check_correct_count_cowsAndBulls10() {
-        Assertions.assertEquals(0,getCowsAndBulls("00002","1112")[0]);
-        Assertions.assertEquals(1,getCowsAndBulls("00002","1112")[1]);
-    }
-    @Test
-    public void check_correct_count_cowsAndBulls11() {
-        Assertions.assertEquals(3,getCowsAndBulls("0209","0009")[0]);
-        Assertions.assertEquals(0,getCowsAndBulls("0209","0009")[1]);
-    }
-
 
 }
