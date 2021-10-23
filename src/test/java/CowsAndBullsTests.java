f3e752b134f165da1090c045f6ae7c1bfe621497
Roman Nechaev <70903393+RomanNechaev@users.noreply.github.com>
2021-10-23 21:40:29 +0500

    Refactor tests

--- src/test/java/CowsAndBullsTests.java
+++ src/test/java/CowsAndBullsTests.java
@@ -35,23 +35,6 @@ public class CowsAndBullsTests {
         Assertions.assertEquals(expectedBulls, cowsAndBulls[1]);
     }
 
-    @Test
-    public void check_correct_count_cowsAndBulls() {
-        Assertions.assertAll(
-                () -> Assertions.assertArrayEquals(new int[]{3, 0}, getCowsAndBulls("гигант", "жигало")),
-                () -> Assertions.assertArrayEquals(new int[]{1, 0}, getCowsAndBulls("гигант", "город")),
-                () -> Assertions.assertArrayEquals(new int[]{0, 1}, getCowsAndBulls("катп", "анас")),
-                () -> Assertions.assertArrayEquals(new int[]{2, 1}, getCowsAndBulls("палка", "лейка")),
-                () -> Assertions.assertArrayEquals(new int[]{2, 2}, getCowsAndBulls("окурок", "крутой")),
-                () -> Assertions.assertArrayEquals(new int[]{0, 4}, getCowsAndBulls("лепс", "спел")),
-                () -> Assertions.assertArrayEquals(new int[]{0, 0}, getCowsAndBulls("    ", "спел")),
-                () -> Assertions.assertArrayEquals(new int[]{0, 4}, getCowsAndBulls("5522", "2255")),
-                () -> Assertions.assertArrayEquals(new int[]{2, 0}, getCowsAndBulls("2222", "2255")),
-                () -> Assertions.assertArrayEquals(new int[]{1, 0}, getCowsAndBulls("0003", "1323")),
-                () -> Assertions.assertArrayEquals(new int[]{0, 1}, getCowsAndBulls("00002", "1112")),
-                () -> Assertions.assertArrayEquals(new int[]{3, 0}, getCowsAndBulls("0209", "0009")));
-    }
-
     @BeforeEach
     public void setUp() {
         game = new CowsAndBullsState();
@@ -61,6 +44,10 @@ public class CowsAndBullsTests {
     public void check_common_cases() {
         check_counting_cows_and_bulls("гигант", "жигало", 3, 0);
         check_counting_cows_and_bulls("гигант", "город", 1, 0);
+        check_counting_cows_and_bulls("палка", "лейка", 2, 1);
+        check_counting_cows_and_bulls("окурок", "крутой", 2, 2);
+        check_counting_cows_and_bulls("1242","2322",1,1);
+        check_counting_cows_and_bulls("0003","1323",1,0);
     }
 
     @Test
@@ -68,5 +55,31 @@ public class CowsAndBullsTests {
         check_counting_cows_and_bulls("    ", "спел", 0,0);
     }
 
-    //...
+    @Test
+    public void check_if_user_length_longer_word() {
+        check_counting_cows_and_bulls("деревья","дерево",5,0);
+    }
+
+    @Test
+    public void check_if_user_message_is_palindrome() {
+        check_counting_cows_and_bulls("лепс","спел",0,4);
+        check_counting_cows_and_bulls("2255","5522",0,4);
+    }
+
+    @Test
+    public void check_if_one_letter_is_missing() {
+        check_counting_cows_and_bulls("ка та","карта",4,0);
+    }
+
+    @Test
+    public void check_if_word_with_letter() {
+        check_counting_cows_and_bulls("сл0н","слон",3,0);
+    }
+
+    @Test
+    public void check_if_word_of_same_letters(){
+        check_counting_cows_and_bulls("ааааа","буква",1,0);
+    }
+
+
 }
